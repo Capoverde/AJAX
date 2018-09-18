018fe099b67c7a40f754ddf292d7fec78b3ad7f4
Piotr <piotr_as@yahoo.com>
2018-09-18 14:51:22 +0200

    external API + popraiwć zmienne globalne

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -1,7 +1,7 @@
 // ZMIENNE GLOBALNE
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn4');
+const btnGH = document.getElementById('btn4');
 const btn4 = document.getElementById('btn5');
 const btn5 = document.getElementById('btn3');
 const btnClear = document.getElementById('clear');
@@ -13,99 +13,100 @@ let user = document.getElementById('user');
 let users = document.getElementById('users');
 
 // Event Listeners
-btn1.addEventListener('click', loadText);
-btn2.addEventListener('click', loadUser);
-btn3.addEventListener('click', loadUsers);
+// btn1.addEventListener('click', loadText);
+// btn2.addEventListener('click', loadUser);
+// btn3.addEventListener('click', loadUsers);
+btnGH.addEventListener('click', loadGHusers);
 btnClear.addEventListener('click', clearContnent);
-btnClear2.addEventListener('click', clearContent3);
+// btnClear2.addEventListener('click', clearContent3);
 
 
-function loadText() {
-    //Create XHR Object
-    var xhr = new XMLHttpRequest();
-    // OPEN - type, url/text, async
-    xhr.open('GET', './root/sample.txt', true);
-    console.log('READYSTATE: ', xhr.readyState);
+// function loadText() {
+//     //Create XHR Object
+//     var xhr = new XMLHttpRequest();
+//     // OPEN - type, url/text, async
+//     xhr.open('GET', './root/sample.txt', true);
+//     console.log('READYSTATE: ', xhr.readyState);
 
 
-    // optional - used for loaders
+//     // optional - used for loaders
 
-    // xhr.onprogress = function () {
-    //     console.log('READYSTATE: ', xhr.readyState);
-    // }
+//     // xhr.onprogress = function () {
+//     //     console.log('READYSTATE: ', xhr.readyState);
+//     // }
 
-    xhr.onload = function () {
-        console.log('READYSTATE: ', xhr.readyState);
-        if (this.status == 200) {
-            // console.log(this.responseText);
+//     xhr.onload = function () {
+//         console.log('READYSTATE: ', xhr.readyState);
+//         if (this.status == 200) {
+//             // console.log(this.responseText);
 
-            container1.innerHTML = this.responseText;
-        } else
-        if (this.status = 404) {
-            container.innerHTML = 'not found... :(';
-            container.style.color = 'red';
-            container.style.fontSize = '2em';
-        }
-    }
+//             container1.innerHTML = this.responseText;
+//         } else
+//         if (this.status = 404) {
+//             container.innerHTML = 'not found... :(';
+//             container.style.color = 'red';
+//             container.style.fontSize = '2em';
+//         }
+//     }
 
-    // xhr.onerror = function () {
-    //     console.log('request error...');
+//     // xhr.onerror = function () {
+//     //     console.log('request error...');
 
-    // }
+//     // }
 
-    // xhr.onreadystatechange = function () {
-    //     console.log('READYSTATE: ', xhr.readyState);
-    //     if (this.readyState == 4 && this.status == 200) {
-    //         console.log(this.responseText);
-    //     }
-    // }
-    // sends request
-    xhr.send();
-}
-
-
-function loadUser() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', './root/scripts/user.json', true)
-
-    xhr.onload = function () {
-        if (this.status == 200) {
-            var user = JSON.parse(this.responseText);
-            var output = '';
-            output += '<ul>' + '<li>ID: ' + user.id + '</li>' +
-                '<li>name: ' + user.name + '</li>' +
-                '<li>email: ' + user.email + '</li>' +
-                '</ul>';
-            document.getElementById('cont2').innerHTML = output;
-        }
-
-    }
-    xhr.send();
-}
+//     // xhr.onreadystatechange = function () {
+//     //     console.log('READYSTATE: ', xhr.readyState);
+//     //     if (this.readyState == 4 && this.status == 200) {
+//     //         console.log(this.responseText);
+//     //     }
+//     // }
+//     // sends request
+//     xhr.send();
+// }
 
 
-function loadUsers() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', './root/scripts/users.json', true);
+// function loadUser() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', './root/scripts/user.json', true)
 
-    xhr.onload = function () {
-        if (this.status == 200) {
-            var users = JSON.parse(this.responseText);
-            var output = '';
-            // Ponieaż jest to tablica, to musimy zrobić pętlę i iterować
-            for (var i in users) {
-                output += '<ul>' +
-                    '<li>ID: ' + users[i].id + '</li>' +
-                    '<li>name: ' + users[i].name + '</li>' +
-                    '<li>email: ' + users[i].mail + '</li>' + '<br>' +
-                    '</ul>';
-            }
-            document.getElementById('cont2').innerHTML = output;
-        }
+//     xhr.onload = function () {
+//         if (this.status == 200) {
+//             var user = JSON.parse(this.responseText);
+//             var output = '';
+//             output += '<ul>' + '<li>ID: ' + user.id + '</li>' +
+//                 '<li>name: ' + user.name + '</li>' +
+//                 '<li>email: ' + user.email + '</li>' +
+//                 '</ul>';
+//             document.getElementById('cont2').innerHTML = output;
+//         }
 
-    }
-    xhr.send();
-}
+//     }
+//     xhr.send();
+// }
+
+
+// function loadUsers() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', './root/scripts/users.json', true);
+
+//     xhr.onload = function () {
+//         if (this.status == 200) {
+//             var users = JSON.parse(this.responseText);
+//             var output = '';
+//             // Ponieaż jest to tablica, to musimy zrobić pętlę i iterować
+//             for (var i in users) {
+//                 output += '<ul>' +
+//                     '<li>ID: ' + users[i].id + '</li>' +
+//                     '<li>name: ' + users[i].name + '</li>' +
+//                     '<li>email: ' + users[i].mail + '</li>' + '<br>' +
+//                     '</ul>';
+//             }
+//             document.getElementById('cont2').innerHTML = output;
+//         }
+
+//     }
+//     xhr.send();
+// }
 
 // Clears content in contaier 
 
@@ -134,3 +135,31 @@ function clearContnent(e) {
 
 
 //  ----------------- EXTERNAL API ------------------ //
+
+
+
+function loadGHusers() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users', true);
+    xhr.onload = function () {
+        if (this.status == 200) {
+            var users = JSON.parse(this.responseText);
+
+            var output = '';
+            for (var i in users) {
+                output +=
+                    '<div class ="GHusers">' +
+                    '<img src="' +
+                    users[i].avatar_url + '" width="70" height="70">' +
+                    '<ul>' +
+                    '<li>ID: ' + users[i].id + '</li>' +
+                    '<li>ID: ' + users[i].login + '</li>' +
+                    '</ul>' +
+                    '</div>';
+            }
+            document.getElementById('cont3').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+}
