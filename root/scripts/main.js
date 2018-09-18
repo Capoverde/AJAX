221c3eb8e30c4138ccd3f3ee0dbab4eb364cbfd0
Piotr <piotr_as@yahoo.com>
2018-09-18 10:02:53 +0200

    starsza wersja 'onreadystatechange'

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -8,16 +8,33 @@ function loadText() {
     // OPEN - type, url/text, async
     xhr.open('GET', './root/sample.txt', true);
 
-    xhr.onload = function () {
-        if (this.status == 200) {
-            console.log(this.responseText);
+    // xhr.onload = function () {
+    //     if (this.status == 200) {
+    //         console.log(this.responseText);
+    //     }
+    // }
 
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
         }
     }
     // sends request
     xhr.send();
 
 
-
-
 }
+
+
+// readystate values:
+//  0 : request not initialized
+//  1: server connection established
+//  2: request recieved
+//  3: processing request
+//  4: request finished and response is ready
+
+
+//  HTTP REQUESTS:
+// 200: "OK"
+// 403: 'FORBIDDEN'
+// 404: "NO FOUND"
